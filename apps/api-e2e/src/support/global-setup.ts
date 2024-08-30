d61a4f9208c14484d76aee4ceae66e386b679de1
Julien Moulin <neilnilom@gmail.com>
2024-08-30 07:03:52 +0200

    test(api): added jest api startup for e2e tests

--- apps/api-e2e/src/support/global-setup.ts
+++ apps/api-e2e/src/support/global-setup.ts
@@ -1,10 +1,22 @@
 /* eslint-disable */
-var __TEARDOWN_MESSAGE__: string;
+import { spawn } from 'child_process';
 
 module.exports = async function () {
   // Start services that that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
 
+  // Start the API server
+  const server = spawn('nx', ['serve', 'api'], {
+    shell: true,
+    stdio: 'inherit',
+  });
+
+  // Store the server process in globalThis so it can be accessed in globalTeardown
+  globalThis.__SERVER_PROCESS__ = server;
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
+
+  // You might want to wait for the server to be fully up before proceeding
+  // This is a simplistic approach; consider polling a health endpoint instead
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 };
